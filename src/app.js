724190957755ac6515d46b75eeeb208bbf8bf089
Diego Pisfil <fdiego.prios@gmail.com>
2022-01-05 18:00:14 -0500

    facebook

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const passport = require('passport')
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
-var MongoStore = require('connect-mongo')(session); 
+var MongoStore = require('connect-mongo')(session);
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const path = require('path')
@@ -17,17 +17,27 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
-app.use(session({
+
+
+var sess = {
     secret: 'keysecret',
     resave: false,
     saveUninitialized: true,
-    store: new MongoStore({ 
+    store: new MongoStore({
         url: process.env.MONGODB_URL,
         ttl: 14 * 24 * 60 * 60,
         autoRemove: 'native'
-    })
-}))
-app.use(bodyParser.urlencoded({extended: true}))
+    }),
+    cookie: {}
+}
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sess.cookie.secure = true // serve secure cookies
+}
+
+app.use(session(sess))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(passport.initialize());
 app.use(passport.session());
 
